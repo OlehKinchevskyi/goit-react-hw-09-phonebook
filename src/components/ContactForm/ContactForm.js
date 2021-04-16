6b85a44430e8b9e7e5c8d573b063fba961131d4c
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2021-04-16 22:22:16 +0300

    Додає валідацію

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -78,6 +78,9 @@ export default function ContactForm() {
                         className="Input"
                         onChange={handleChange}
                         name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                        required
                         
                     />
             
@@ -91,6 +94,9 @@ export default function ContactForm() {
                         className="Input"
                         onChange={handleChange}
                         name="number"
+                        pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+                        title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+                        required
                         />
                     
                     <button
